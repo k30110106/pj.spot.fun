b46cab68cf2fcb35f5970f5df8adba26b1544378
jklasd1472 <jklasd1472@gmail.com>
2024-12-09 18:11:03 +0900

    ,
    (cherry picked from commit 3deecb7d96e33942e2512a90142660af8c4247c8)

--- frontend/src/usr/signup/page/SocialSignupPage.jsx
+++ frontend/src/usr/signup/page/SocialSignupPage.jsx
@@ -5,11 +5,13 @@ import SocialSignupComponent from "../component/SocialSignupComponent";
 
 function SocialSignupPage() {
   return (
-    <div id="Singup-page">
-      <Header />
-      회원가입
-      <SocialSignupComponent />
-      <Footer />
+    <div className="custom-cursor">
+      <div id="Singup-page">
+        <Header />
+        <h1 className="flex flex-col justify-center items-center text-2xl font-bold m-10">회원가입</h1>
+        <SocialSignupComponent />
+        <Footer />
+      </div>
     </div>
   );
 }
