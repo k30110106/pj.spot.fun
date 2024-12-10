8e64ffbf5f55c6f827ddf0aac3d579c190765a95
Dong <k30110106@gmail.com>
2024-12-10 18:12:02 +0900

    마이페이지 좋아요메뉴 작업
    (cherry picked from commit 1c08d6cb83c757041c282163379b95227c1b4938)

--- backend/src/main/java/com/spot/fun/usr/feed/repository/like/UserFeedLikeRepository.java
+++ backend/src/main/java/com/spot/fun/usr/feed/repository/like/UserFeedLikeRepository.java
@@ -1,10 +1,26 @@
 package com.spot.fun.usr.feed.repository.like;
 
+import com.spot.fun.usr.feed.dto.FeedRequestDTO;
 import com.spot.fun.usr.feed.entity.like.FeedLike;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserFeedLikeRepository extends JpaRepository<FeedLike, Long> {
-    Long countByFeedIdx(Long feedIdx);
-    boolean existsByFeedIdxAndUserIdx(Long feedIdx, Long userIdx);
-    void deleteByFeedIdxAndUserIdx(Long feedIdx, Long userIdx);
+  Long countByFeedIdx(Long feedIdx);
+
+  boolean existsByFeedIdxAndUserIdx(Long feedIdx, Long userIdx);
+
+  void deleteByFeedIdxAndUserIdx(Long feedIdx, Long userIdx);
+
+  @Query("SELECT f FROM FeedLike f " +
+          "INNER JOIN f.feed feed " +
+          "WHERE f.user.idx = :#{#feed.userIdx} " +
+          "AND (:#{#feed.lastId}=0 or f.idx < :#{#feed.lastId})")
+  List<FeedLike> findFeedsByUserIdxOrderByIdxDesc(@Param("feed") FeedRequestDTO feedRequestDTO, Pageable pageable);
+
+
 }
--- backend/src/main/java/com/spot/fun/usr/feed/service/like/UserFeedLikeService.java
+++ backend/src/main/java/com/spot/fun/usr/feed/service/like/UserFeedLikeService.java
@@ -1,6 +1,11 @@
 package com.spot.fun.usr.feed.service.like;
 
+import com.spot.fun.usr.feed.dto.FeedRequestDTO;
+import com.spot.fun.usr.feed.dto.FeedResponseDTO;
+
 public interface UserFeedLikeService {
   boolean insert(Long idx, Long userIdx);
   boolean delete(Long idx, Long userIdx);
+
+  FeedResponseDTO getLikeListByMypage(FeedRequestDTO feedRequestDTO);
 }
