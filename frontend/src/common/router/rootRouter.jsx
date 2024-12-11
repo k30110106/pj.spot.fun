94252216ac0af6d60b7c6c6e7535443709e1364e
jklasd1472 <jklasd1472@gmail.com>
2024-12-11 14:45:54 +0900

    데이트 코스 및 프론트 수정中
    (cherry picked from commit 727f9542c143e6024a8e284e31e074ab73a98ff2)

--- frontend/src/common/router/rootRouter.jsx
+++ frontend/src/common/router/rootRouter.jsx
@@ -30,6 +30,7 @@ const DateCourseListPage = lazy(() =>
 );
 // const AddDatePage = lazy(() => import("../../usr/course/page/AddDatePage")); // 수정된 부분
 const ChatListPage = lazy(() => import("../../usr/chat/page/ChatListPage"));
+const AddCoursePage = lazy(() => import("../../usr/course/page/AddCoursePage")); // 수정된 부분
 const SignupCheckPage = lazy(() =>
   import("../../usr/signup/page/SignupCheckPage")
 );
@@ -122,6 +123,14 @@ const rootRouter = createBrowserRouter(
         </Suspense>
       ),
       children: dateRouter(), // DateRouter 자식 라우터 추가
+    },
+    {
+      path: "addcourse",
+      element: (
+        <Suspense fallback={Loading}>
+          <AddCoursePage />
+        </Suspense>
+      ),
     },
     // {
     //   path: "addDate",
